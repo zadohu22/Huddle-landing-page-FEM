ed76b5c81bcaf926bdb31ac650a7c8146b9547d2
zadohu22 <zadohu22@gmail.com>
2022-11-06 22:56:24 -0600

    fixes mobile spacing take two

--- src/components/AppWrap.jsx
+++ src/components/AppWrap.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppWrap = (props) => {
   return (
-    <div className="h-[100%] w-full flex flex-col lg:flex-row justify-center items-center bg-[url('./images/images/bg-mobile.svg')] sm:bg-[url('./images/images/bg-desktop.svg')] bg-no-repeat bg-violetHuddle relative">
+    <div className="min-h-full w-full flex flex-col lg:flex-row justify-center items-center bg-[url('./images/images/bg-mobile.svg')] sm:bg-[url('./images/images/bg-desktop.svg')] bg-no-repeat bg-violetHuddle relative">
       {props.children}
       {console.log("props.children is my new favorite thing.")}
     </div>
